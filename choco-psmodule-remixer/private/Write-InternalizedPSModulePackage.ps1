711c49a1607e952746075f2640094af79477a2d6
hb9chm <stephan.enzler@gmx.ch>
2023-02-12 21:25:47 +0100

    Update Write-InternalizedPSModulePackage.ps1

--- choco-psmodule-remixer/private/Write-InternalizedPSModulePackage.ps1
+++ choco-psmodule-remixer/private/Write-InternalizedPSModulePackage.ps1
@@ -17,14 +17,13 @@
         $internalizedXMLcontent.save($internalizedXMLPath)
 
         [XML]$internalizedXMLcontent = Get-Content $internalizedXMLPath
-    }
-
-    # no duplicate versions
-    if (-not ($internalizedXMLcontent.SelectSingleNode("//pspkg[@id=""$nuspecID""]/version[""$version""]"))) {
-        Write-Verbose "adding $nuspecID $version to list of internalized packages"
-        $addVersion = $internalizedXMLcontent.CreateElement("version")
-        $null = $addVersion.AppendChild($internalizedXMLcontent.CreateTextNode("$version"))
-        $internalizedXMLcontent.SelectSingleNode("//pspkg[@id=""$nuspecID""]").appendchild($addVersion) | Out-Null
-        $internalizedXMLcontent.save($internalizedXMLPath)
+    } else {
+        if (-not ($internalizedXMLcontent.SelectSingleNode("//pspkg[@id=""$nuspecID""]/version[""$version""]"))) {
+            Write-Verbose "adding $nuspecID $version to list of internalized packages"
+            $addVersion = $internalizedXMLcontent.CreateElement("version")
+            $null = $addVersion.AppendChild($internalizedXMLcontent.CreateTextNode("$version"))
+            $internalizedXMLcontent.SelectSingleNode("//pspkg[@id=""$nuspecID""]").appendchild($addVersion) | Out-Null
+            $internalizedXMLcontent.save($internalizedXMLPath)
+        }
     }
 }
